8511ab6b67047451aaf0f039d9ee376676e93ad6
Iskender Shamaldaev <sh.iskander012@gmail.com>
2024-01-09 01:41:06 +0600

    changed apiUrl

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,4 @@
-export const apiUrl = 'http://localhost:8000';
+export const apiUrl = 'https://react-crud-3nye.onrender.com';
 
 export const GOOGLE_CLIENT_ID =
   '296872384803-slj79sujp576fqi6nhppah96l0se9b7o.apps.googleusercontent.com';
