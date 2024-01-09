1b5c89556b2e67521c9ae4acdafcb8c1e0b0b051
Iskender Shamaldaev <sh.iskander012@gmail.com>
2024-01-09 19:05:21 +0600

    changed index.ts

--- api/index.ts
+++ api/index.ts
@@ -14,9 +14,23 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://app-cocktail-recipes.netlify.app');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.header('Access-Control-Allow-Credentials');
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://app-cocktail-recipes.netlify.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Private-Network", "true");
+
+  res.setHeader("Access-Control-Max-Age", "7200");
+
   next();
 });
 
